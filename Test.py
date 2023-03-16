f2fe5fde5ce648ddb8b29201be1edc03df996c3a
Maruskaa <majoneza37@gmail.com>
2023-03-16 15:32:00 +0100

    Expanded for multiple products types Required to repair "Cenik" table (Not adding)

--- Test.py
+++ Test.py
@@ -2,21 +2,45 @@ import sqlite3
 conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-#conn.execute("""
-# CREATE TABLE Napoje (
-#     ID INTEGER PRIMARY KEY,
-#     Name VARCHAR(255),
-#     Ingredients TEXT
-# );
-# """)
+conn.execute("""
+CREATE TABLE Pizza (
+    ID INTEGER PRIMARY KEY,
+    Name VARCHAR(255),
+    Ingredients TEXT
+);
+""")
 
-cursor.execute(f"INSERT INTO Napoje (NAME, INGREDIENTS) \
-        VALUES (?,?)", ('Kola', 'Tomato, Cheese'))
-conn.commit()
+conn.execute("""
+CREATE TABLE Napoje (
+    ID INTEGER PRIMARY KEY,
+    Name VARCHAR(255),
+    Ingredients TEXT
+);
+""")
+
+conn.execute("""
+CREATE TABLE Deserty (
+    ID INTEGER PRIMARY KEY,
+    Name VARCHAR(255),
+    Ingredients TEXT
+);
+""")
+
+conn.execute("""
+CREATE TABLE Cenik (
+    ID INTEGER PRIMARY KEY,
+    Name VARCHAR(255),
+    Price INT
+);
+""")
+
+# cursor.execute(f"INSERT INTO Napoje (NAME, INGREDIENTS) \
+#         VALUES (?,?)", ('Kola', 'Tomato, Cheese'))
+# conn.commit()
 
 
-cursor.execute('''SELECT * FROM Napoje''')
-data = cursor.fetchall()
-print(data)
+# cursor.execute('''SELECT * FROM Napoje''')
+# data = cursor.fetchall()
+# print(data)
 
 conn.close()
--- app.py
+++ app.py
@@ -10,55 +10,85 @@ print("Opened database successfully")
 
 
 def get_all(typ: str):
-    cursor.execute(f"select * from {typ}")
-    data = cursor.fetchall()
-    return data
+    try:
+        cursor.execute(f"select * from {typ}")
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        return f"ERROR | {e}"
 
 
 def search(query: str, typ: str):
-    cursor.execute(f"SELECT * FROM {typ} WHERE NAME LIKE '%'||?||'%'", (query,))
-    data = cursor.fetchall()
-    return data
+    try:
+        cursor.execute(f"SELECT * FROM {typ} WHERE NAME LIKE '%'||?||'%'", (query,))
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        return f"ERROR | {e}"
 
 
-def pizza_insert(name: str, ingredients: str):
-    cursor.execute(f"INSERT INTO Pizza (NAME, INGREDIENTS) \
-            VALUES (?,?)", (name, ingredients))
-    conn.commit()
+def insert(name: str, ingredients: str, price: str, typ: str):
+    try:
+        if name and ingredients and not price:
+            cursor.execute(f"INSERT INTO {typ} (NAME, INGREDIENTS) \
+                    VALUES (?,?)", (name, ingredients))
+            conn.commit()
+        elif name and price and not ingredients:
+            cursor.execute(f"INSERT INTO Cenik (NAME, INGREDIENTS) \
+                        VALUES (?,?)", (name, ingredients))
+            conn.commit()
+        else:
+            return f"ERROR | Given parameters are not complete"
+    except Exception as e:
+        return f"ERROR | {e}"
 
 
-def delete_by_id(id: int, typ: str):
-    cursor.execute(f"DELETE FROM {typ} WHERE id = {id}")
+def delete_by_id(ID: int, typ: str):
+    cursor.execute(f"DELETE FROM {typ} WHERE id = {ID}")
     conn.commit()
 
 
 @app.route('/<typ>')
 def pizza(typ):
+    typ = typ.capitalize()
     return json.dumps(get_all(typ))
 
 
-@app.route('/pizza/add', methods=['POST'])
-def pizza_add():
+@app.route('/<typ>/add', methods=['POST'])
+def pizza_add(typ):
+    typ = typ.capitalize()
     if request.method == 'POST':
-        name = request.form.get('name')
-        ingredients = request.form.get('ingredients')
-        pizza_insert(name, ingredients)
-        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        try:
+            name = request.form.get('name')
+            ingredients = request.form.get('ingredients')
+            price = request.form.get('price')
+            insert(typ=typ, name=name, ingredients=ingredients, price=price)
+            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        except Exception as e:
+            return json.dumps({'success': False, 'ERROR': f'{e}'}), 200, {'ContentType': 'application/json'}
 
 
-@app.route('/pizza/delete', methods=['POST'])
-def pizza_delete():
+@app.route('/<typ>/delete', methods=['POST'])
+def pizza_delete(typ):
+    typ = typ.capitalize()
     if request.method == 'POST':
-        id = request.form.get('id')
-        delete_by_id(id=id, typ="Pizza")
-        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        try:
+            id = request.form.get('id')
+            delete_by_id(ID=id, typ=typ)
+            return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        except Exception as e:
+            return json.dumps({'success': False, 'ERROR': f'{e}'}), 200, {'ContentType': 'application/json'}
 
 
-@app.route('/pizza/search', methods=['POST'])
-def pizza_search():
+@app.route('/<typ>/search', methods=['POST'])
+def pizza_search(typ: str):
+    typ = typ.capitalize()
     if request.method == 'POST':
-        query = request.form.get('query')
-        return json.dumps(search(query, typ='Pizza'))
+        try:
+            query = request.form.get('query')
+            return json.dumps(search(query, typ=typ))
+        except Exception as e:
+            return json.dumps({'success': False, 'ERROR': f'{e}'}), 200, {'ContentType': 'application/json'}
 
 
-app.run(host="localhost")
+app.run(debug=False)
